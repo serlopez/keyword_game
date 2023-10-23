6e233bae5ff3e9e1ae309d3cb095d9357f3eb56f
Sergio Lopez <sergiolopezpdx@gmail.com>
2023-10-22 20:14:04 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let trashwordNum = 0;
 let score = 0;
 let questionIndex = 0;
 
-let phrases = ["The five boxing wizards jump quickly.",
+let phrases = ['<span class="trashword">this </span><span class="trashword">is </span><span class="trashword">a </span><span class="keyword">test.</span>',
 "Pack my box with five dozen liquor jugs.",
 "Go, lazy fat vixen; be shrewd, jump quick.",
 "When zombies arrive, quickly fax Judge Pat.",
@@ -18,7 +18,7 @@ let phrases = ["The five boxing wizards jump quickly.",
 
 let button = document.getElementById("next_phrase");
 button.addEventListener('click', function(){
-    game(questionIndex);
+    game();
 });
 
 function game()
@@ -30,38 +30,42 @@ function game()
         button.disabled = true;
     }
     else{
-        let phrase = document.getElementById("phrase_display");
         let trashwords = document.getElementsByClassName("trashword");
         let keywords = document.getElementsByClassName("keyword");
+        button.disabled = true;
+        score = 0;
 
         phrase.innerHTML = phrases[questionIndex];
-        document.getElementById("score").innerText = "score: " + score;
+        let scoreDisplay = document.getElementById("score");
+        scoreDisplay.innerText = "There are " + keywords.length + " keywords in this sentence. You have found " + score +" percent of them.";
 
         for (let i = 0; i < trashwords.length; i++){
             trashwords[i].addEventListener('click', function(){
                 trashwords[i].classList.add("trashwordFound");
-                trashwordNum += 1;
-                calculateScore();
             }, {once: true});
         }
         for (let i = 0; i < keywords.length; i++){
             keywords[i].addEventListener('click', function(){
                 keywords[i].classList.add("keywordFound");
                 keywordNum += 1;
-                calculateScore();
+                calculateScore(keywords, scoreDisplay);
             }, {once: true});
         }
         questionIndex += 1;
     }    
 }
 
-function calculateScore(){
-    score = keywordNum == 0 ? 0 : (keywordNum / (keywordNum + trashwordNum))*100;
-    document.getElementById("score").innerText = "score: " + score + " percent";
-
+function calculateScore(keywords, scoreDisplay){
+    score = keywordNum / keywords.length * 100;
+    scoreDisplay.innerText = "There are " + keywords.length + " keywords in this sentence. You have found " + score +" percent of them.";
+    if (keywordNum == keywords.length){
+        scoreDisplay.innerText = "Woohoo!";
+        button.disabled = false;
+        keywordNum = 0;
+    }
 }
 
 
 
-game(questionIndex);
+game();
 

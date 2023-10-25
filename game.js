f8ea65e2b70bba0f254554c327d4f27f001387f6
Sergio Lopez <sergiolopezpdx@gmail.com>
2023-10-24 17:29:35 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ let keywordNum = 0;
 let trashwordNum = 0;
 let score = 0;
 let questionIndex = 0;
+let count = 60;
+let timer;
 
-let phrases = ['<p><span class="trashword">The</span> <span class="trashword">quick</span> <span class="trashword">brown</span> <span class="keyword">fox</span> <span class="keyword">jumps</span> <span class="keyword">over</span> <span class="trashword">the</span> <span class="trashword">lazy</span> <span class="keyword">dog</span></p>',
+let phrases = ['<p><span class="trashword">The</span> <span class="trashword">quick</span> <span class="trashword">brown</span> <span class="keyword">fox</span> <span class="keyword">jumps</span> <span class="keyword">over</span> <span class="trashword">the</span> <span class="trashword">lazy</span> <span class="keyword">dog</span>.</p>',
 '<p><span class="keyword">Pack</span> <span class="trashword">my</span> <span class="keyword">box</span> <span class="keyword">with</span> <span class="trashword">five</span> <span class="trashword">dozen</span> <span class="keyword">liquor</span> <span class="keyword">jugs</span>.</p>',
 '<p><span class="keyword">Go</span>, <span class="trashword">lazy</span> <span class="trashword">fat</span> <span class="keyword">vixen</span>; <span class="keyword">be</span> <span class="keyword">shrewd</span>, <span class="keyword">jump</span> <span class="trashword">quick</span>.</p>',
 '<p><span class="keyword">When</span> <span class="keyword">zombies</span> <span class="keyword">arrive</span>, <span class="trashword">quickly</span> <span class="keyword">fax</span> <span class="keyword">Judge</span> <span class="keyword">Pat</span>.</p>',
@@ -18,7 +20,13 @@ let phrases = ['<p><span class="trashword">The</span> <span class="trashword">qu
 
 let button = document.getElementById("next_phrase");
 button.addEventListener('click', function(){
+    count = 60;
     game();
+});
+
+let hintButton = document.getElementById("hint");
+hintButton.addEventListener('click', function(){
+    alert("While finding keywords is not an exact science, nouns, verbs, prepositions and conjunctions make good keywords, while pronouns, adjectives, adverbs, and articles do not.");
 });
 
 function game()
@@ -28,16 +36,30 @@ function game()
         console.log("game over");
         phrase.innerHTML = phrases[phrases.length - 1];
         button.disabled = true;
+        scoreDisplay.innerText = 'Your final score is: ' + keywordNum;
     }
     else{
         let trashwords = document.getElementsByClassName("trashword");
         let keywords = document.getElementsByClassName("keyword");
         button.disabled = true;
+        phrase.classList.remove('disabled');
         score = 0;
 
         phrase.innerHTML = phrases[questionIndex];
         let scoreDisplay = document.getElementById("score");
         scoreDisplay.innerText = "There are " + keywords.length + " keywords in this sentence. You have found " + score +" percent of them.";
+
+        timer = setInterval(function(){
+            count--;
+            console.log(count);
+            document.getElementById('timer').innerHTML = "Time left: " + count + " seconds.";
+            if (count == 0){
+                clearInterval(timer);
+                keywordNum = keywords.length;
+                calculateScore(keywords, trashwords, scoreDisplay);
+                phrase.classList.add('disabled');
+            }
+        }, 1000);
 
         for (let i = 0; i < trashwords.length; i++){
             trashwords[i].addEventListener('click', function(){
@@ -52,23 +74,25 @@ function game()
             }, {once: true});
         }
         questionIndex += 1;
-    }    
-}
-
-function calculateScore(keywords, trashwords, scoreDisplay){
-    score = keywordNum / keywords.length * 100;
-    scoreDisplay.innerText = "There are " + keywords.length + " keywords in this sentence. You have found " + score +" percent of them.";
-    if (keywordNum == keywords.length){
-        for (let i = 0; i < trashwords.length; i++){
-            trashwords[i].style.visibility = 'hidden';
-        }
-        scoreDisplay.innerText = "Woohoo!";
-        button.disabled = false;
-        keywordNum = 0;
     }
 }
 
+function calculateScore(keywords, trashwords, scoreDisplay){
+    score = Math.round(keywordNum / keywords.length * 100);
+    scoreDisplay.innerText = "There are " + keywords.length + " keywords in this sentence. You have found " + score +" percent of them.";
+    if (keywordNum == keywords.length){
+        timesUp(trashwords, scoreDisplay);
+    }
+}
 
-
+function timesUp(trashwords, scoreDisplay){
+    clearInterval(timer);
+    for (let i = 0; i < trashwords.length; i++){
+        trashwords[i].style.visibility = 'hidden';
+    }
+    scoreDisplay.innerText = "Woohoo!";
+    button.disabled = false;
+    keywordNum = 0;
+}
 game();
 
